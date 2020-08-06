bc01e8c20e982519c19424e9cb049c6c0e4c5eaa
Glitch (hello-express) <none>
2020-08-06 06:03:33 +0000

    ⛷🐒 Checkpoint ./routes/tracker.router.js:10884473/1513

--- routes/tracker.router.js
+++ routes/tracker.router.js
@@ -26,26 +26,35 @@ router.get('/users', (req, res) => {
     .catch(err => res.json(err) );
 });
 
-router.post('/add', (req, res) => {
-  // because the date is optionally, we need to test whether date is there or not
-  let date;
-  if(req.body.date){
-    date = req.body.date;
-  } else {
-    date = new Date();
+router.post('/add', async (req, res) => {
+   const user = await User.findById(req.body.userId);
+  if(!user) {
+    res.send("userId " + req.body.userId + " not found");
+    return;
+  } 
+  const exercise = new User({ 
+    description: req.body.description,
+    duration: req.body.duration
+  });
+  // if a date was provided, set it in the exercise
+  //  otherwise MongoDB will default it to Date.now()
+  if(req.body.date) exercise.date = req.body.date
+  user.log.push(exercise);
+  
+  try {
+    await user.save();
+    // res.json(user);
+    res.json({
+      _id: user._id,
+      username: user.username,
+      description: exercise.description,
+      duration: exercise.duration,
+      date: exercise.date
+    })
+  } catch(e) {
+    console.log(e);
+    res.send("Sorry, there was an error saving the exercise.");
   }
-
-  // create the excercise which will be save
-  const logger = {
-    description: req.body.description, 
-    duration: req.body.duration, 
-    date: date
-  };
-  console.log('logger',logger)
-  // finally we find the user by his id and we update him
-  User.findByIdAndUpdate(req.body.userId, {$push: { log: logger}}, {new: true}).exec()
-    .then( user => res.json({id: user.id, username: user.username, log: user.log[user.log.length-1]}))
-    .catch( err => res.json(err) );
 });
 
 
