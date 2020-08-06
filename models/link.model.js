cfe821f9c7e132b772396424208137ccb3fa8663
Glitch (hello-express) <none>
2020-08-06 02:30:44 +0000

    💠🌕 Checkpoint ./routes/link.router.js:10884473/3138 ./package.json:10884473/3149 ./models/link.model.js:10884473/236

---
+++ models/link.model.js
@@ -0,0 +1,9 @@
+const mongoose  = require('mongoose');
+const Schema = mongoose.Schema;
+const urlSchema = new Schema({
+ original_url: String,
+ short_url: String
+})
+const URL = mongoose.model('URL', urlSchema);
+
+export default 
--- routes/link.router.js
+++ routes/link.router.js
@@ -1,9 +1,44 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
-// About page route.
-router.get('/new', function (req, res) {
-  res.json({message: 'hahahha'});
-})
+var validUrl = require("valid-url");
+const shortId = require('shortid');
+const URL = require("../models/")
+router.get("/new", async (req, res) => {
+  const url = req.body.url_input;
+  const urlCode = shortId.generate();
+  // check if the url is valid or not
+  if (!validUrl.isWebUri(url)) {
+    res.status(401).json({
+      error: "invalid URL"
+    });
+  } else {
+    try {
+      // check if its already in the database
+      let findOne = await URL.findOne({
+        original_url: url
+      });
+      if (findOne) {
+        res.json({
+          original_url: findOne.original_url,
+          short_url: findOne.short_url
+        });
+      } else {
+        // if its not exist yet then create new one and response with the result
+        findOne = new URL({
+          original_url: url,
+          short_url: urlCode
+        });
+        await findOne.save();
+        res.json({
+          original_url: findOne.original_url,
+          short_url: findOne.short_url
+        });
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).json("server erorr…");
+    }
+  }
+});
 
 module.exports = router;
